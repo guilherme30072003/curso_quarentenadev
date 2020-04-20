b17dd5d51118383a997bfe6ffdbf86905dea0d9a
guilherme30072003 <guilherme30072003@gmail.com>
2020-04-19 20:58:57 -0400

    Add link of the layout

--- Aula_5/exercicio2/scripts.js
+++ Aula_5/exercicio2/scripts.js
@@ -1,2 +1,3 @@
 //Exercício2
 //Aprendemos algumas tags HTML e como estilizar essas tags usando css. Será que você é capaz de reproduzir o seguinte layout?
+// Layout: https://codepen.io/felipedotcom/pen/dyoLEEg?editors=0100
